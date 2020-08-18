445d7bb60c24896b96830c67ada235ab1fc353a4
priyankadhulkhed <priyanka.dhulkhed@gmail.com>
2020-08-18 09:39:52 -0400

    set up jest and enzyme

--- src/components/FilterBar/FilterBar.test.js
+++ src/components/FilterBar/FilterBar.test.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+// import { render, fireEvent } from '@testing-library/react';
 import FilterBar from './FilterBar';
+import LoanSize from './NumericFilters/LoanSize';
+import { shallow, mount } from 'enzyme';
 
 describe("Filter Bar Input Form", () => {
-    const setup = () => {
-        const component = render(<FilterBar />);
-        const loanSizeInput = component.getByLabelText('loan-size-input');
-        const submit = component.getByLabelText('submit');
-        return {
-            loanSizeInput,
-            submit,
-            ...component
-        }
-    }
-
-    test('Loan Size input does not accept invalid numbers to be inputted', () => {
-        const { loanSizeInput, submit } = setup();
-        fireEvent.change(loanSizeInput, { target: { value: -50}});
-        fireEvent.click(submit);
-        // expect(handleSubmitSpy.toBeCalled())
+    test('renders properly', () => {
+        const wrapper = shallow(<FilterBar />)
+        expect(wrapper.length).toEqual(1);
     });
 
+    test('Loan Size accepts valid numbers', () => {
+        const wrapper = mount(<FilterBar />);
+        const handleSubmitSpy = jest.spyOn(wrapper.instance(), 'handleSubmit');
+        wrapper.find('[aria-label="loan-size-input"]').simulate('change', { target: {value: 12300000}});
+        wrapper.find('[aria-label="submit"]').simulate('click');
+        wrapper.update();
+        expect(handleSubmitSpy).toBeCalled();
+    });
 });
